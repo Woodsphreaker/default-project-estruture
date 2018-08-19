d2faba76311a0debd5fabcf7b92456bd7143c288
Carlo Enrico <carlo.enrico@uol.com.br>
2018-08-19 20:47:58 -0300

    Update

--- helpers/loadMiddlewares.js
+++ helpers/loadMiddlewares.js
@@ -1,8 +1,8 @@
+const middlewares = require('../middlewares')
 
-module.exports = (app) => {
-  const logRoutes = (req, res, next) => {
-    next()
+module.exports = app => {
+  const allMiddlewares = Object.values(middlewares)
+  for (const middleware of allMiddlewares) {
+    app.use(middleware)
   }
-
-  app.use(logRoutes)
 }
--- helpers/loadModules.js
+++ helpers/loadModules.js
@@ -3,13 +3,9 @@ const fs = require('fs')
 const path = require('path')
 const modules = fs.readdirSync(path.join(__dirname, '../modules'))
 
-// APP Config
-
-const createRoute = app => {
+module.exports = app => {
   for (const mod of modules) {
     const loadModule = require(`../modules/${mod}`)
     app.use(`/${mod}`, loadModule)
   }
 }
-
-module.exports = createRoute
